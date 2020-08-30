91b21fdb69d8113fe099ca41e9b8ac74e57c011d
Aakash Basnet <iamaakashbasnet@gmail.com>
2020-08-30 21:50:44 +0545

    Basic templates and static files

--- tweet/views.py
+++ tweet/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
+tweets = [
+    {
+        'author': 'Steve Smith',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
 def home(request):
-    return HttpResponse('Home')
+    context = {
+        'title': 'Home',
+        'tweets': tweets
+    }
+    return render(request, 'tweet/home.html', context)
 
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'tweet/about.html', {'title': 'About'})
