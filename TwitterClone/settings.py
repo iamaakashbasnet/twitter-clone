066e64e1edfb7281d8e512d6de9a00adecd79d2d
Aakash Basnet <iamaakashbasnet@gmail.com>
2020-08-31 11:15:30 +0545

    User can register

--- TwitterClone/settings.py
+++ TwitterClone/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'tweet.apps.TweetConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
