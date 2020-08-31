cf9c622ecfc7269818f5786bb7885454c835f275
Aakash Basnet <iamaakashbasnet@gmail.com>
2020-08-31 13:08:54 +0545

    User can update profile information

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -11,3 +12,17 @@ class UserRegistrationForm(UserCreationForm):
 
         fields = ['first_name',
                   'last_name', 'username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
