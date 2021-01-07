a2e87ff6839950f9de4c6de575162dba75c87ad7
Aakash Basnet <iamaakashbasnet@gmail.com>
2021-01-07 12:37:08 +0545

    README.md created

---
+++ README.md
@@ -0,0 +1,6 @@
+# Twitter Clone
+Basic clone of twitter made with Django
+
+<p align="center">
+    <img src="./tc.png" width="50%"/>
+</p>
--- TwitterClone/urls.py
+++ TwitterClone/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 from users import views as user_views
 
 urlpatterns = [
-    path('admin_user/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('tweet.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
